381071788ebab17233efa1b71e6e9a73f25a64fc
Pierre Ferré <pierre@pierreferre.com>
2015-10-01 09:29:11 +0100

    just do this in the setup method. no tear down

--- src/AppBundle/Tests/Traits/DatabaseTransactions.php
+++ src/AppBundle/Tests/Traits/DatabaseTransactions.php
@@ -8,12 +8,9 @@ trait DatabaseTransactions
 {
     public function setUp()
     {
-        exec('app/console doctrine:migrations:migrate --no-interaction -e=test');
-    }
-
-    public function tearDown()
-    {
-        exec('app/console doctrine:database:drop -e=test');
-        exec('app/console doctrine:database:create -e=test');
+        exec('app/console doctrine:database:drop --force --env=test');
+        exec('app/console doctrine:database:create --env=test');
+        exec('app/console doctrine:migrations:migrate --no-interaction --env=test');
+        exec('app/console doctrine:fixtures:load --no-interaction --env=test');
     }
 }
